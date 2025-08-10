e21f3bbeec4eb6fe99d4d8071f23ceefa08bb5db
iangelmanuel <iangelmanueldm@gmail.com>
2025-08-10 12:13:45 -0500

    fix: remove unused @astrojs/tailwind dependency from package.json and pnpm-lock.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config"
-import tailwindcss from "@tailwindcss/vite"
 import react from "@astrojs/react"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()]
   }
